a823b4ff98f65c283c2f5b0436a0ea42e7e9dae1
ydevl <ydevlicharov@bk.ru>
2022-03-25 00:54:03 +0300

    1siy

--- src/samples/hash_map.py
+++ src/samples/hash_map.py
@@ -26,6 +26,12 @@ class LinkedList:
             self.last = self.head
             self.length += 1
         else:
+            current = self.head
+            while current is not None:
+                if current.key == key:
+                    current.value = value  #заменяем значение если есть уже похожий ключ
+                    return
+                current = current.next
             self.last.next = Node(value, key)
             self.last = self.last.next
             self.length += 1
@@ -34,7 +40,7 @@ class LinkedList:
         s = 0
         current = self.head
         if current.key == key:
-            self.head = current.next
+            self.head = self.head.next
             self.length -= 1
         else:
             while current.next is not None:
@@ -58,20 +64,87 @@ class LinkedList:
             return print('wrong key, try again')
 
 class HashMap:
-    def __init__(self, _size = 10):
+    def __init__(self, size = 10):
+        self._inner_list = [None] * size
+        self._size = size
+        self._ctr = 0  #counter
 
+    def __getitem__(self, key):
+        linkedlist = self._inner_list[hash(key) % self._size]  #получаем связанный список из заданной ячейки хэшмапы
+        if linkedlist is None:
+            return print('wrong key')
+        while linkedlist.head is not None:
+            if linkedlist.head.key == key:
+                return linkedlist.head.value
+            linkedlist = linkedlist.next
+        return None
 
+    def __setitem__(self, key, value):
+        adress = hash(key) % self._size
+        if self._inner_list[adress] is None:
+            self._inner_list[adress] = LinkedList()
+            self._inner_list[adress].addend(value, key)  #засовываем в пустую ячейку хэшмапы связанный список с ключом и value
+        else:
+            while self._inner_list[adress].head is not None:
+                if self._inner_list[adress].head.key == key:
+                    self._inner_list[adress].head.value = value  #ищем узел с похожим ключом и меняем значение
+                    return
+                self._inner_list[adress].head = self._inner_list[adress].head.next
+            self._inner_list[adress].addend(value, key)
+        self._ctr += 1
+        if self._ctr >= 0.8 * self._size:
+            self._size = self._size * 2 #при увеличении счетчика ключей/значений надо увеличивать размер мапы
+            new_list = [None] * self._size
+            for _ in self._inner_list:
+                if _ is not None:
+                    while _.head is not None:
+                        adress = hash(_.head.key) % self._size
+                        if new_list[adress] is None:
+                            new_list[adress] = LinkedList()
+                            new_list[adress].addend(_.head.key, _.head.value)  #заново перераспределяем ключи/значения
+                        else:
+                            new_list[adress].addend(_.head.key, _.head.value)
+                        _.head = _.head.next
+            self._inner_list = new_list
+
+    def __delitem__(self, key):
+        booly = 0
+        for linkedlist in self._inner_list:
+            if linkedlist is not None:
+                lengthcheck = linkedlist.length
+                linkedlist.delem(key)
+                if linkedlist.length != lengthcheck:
+                    booly = 1
+                    break;
+        if booly == 1:
+            self._cnt -= 1
+            if self._size * 0.8 > self._ctr and self._size > 10:
+                self._size = self._size // 2
+                new_list = [None] * self._size
+                for _ in self._inner_list:
+                    if _ is not None:
+                        while _.head is not None:
+                            adress = hash(_.head.key) % self._size
+                            if new_list[adress] is None:
+                                new_list[adress] = LinkedList()
+                                new_list[adress].addend(_.head.key, _.head.value)  # заново перераспределяем ключи/значения
+                            else:
+                                new_list[adress].addend(_.head.key, _.head.value)
+                            _.head = _.head.next
+                self._inner_list = new_list
+        else:
+            return print('wrong key')
 
 
 arr = LinkedList()
-arr.addend(5, 'dog')
-arr.addend(10, 'cat')
-arr.addend(15, 'elephant')
-arr.addend(25, 'your mama')
-arr.addend(30, 'elephant')
-arr.addend(29, 'elephant')
-arr.addend(39, 'siu')
 
+
+arr.addend(15, 'elephant')
+
+arr.addend(30, 'elephant')
+arr.addend(31, 'elephant')
 arr.delem('elephant')
+
+
 print(arr)
 
