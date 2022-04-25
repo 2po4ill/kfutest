21eb61b2aa79c5c5db6847ff111273f2cea1d969
ydevl <ydevlicharov@bk.ru>
2022-04-26 00:55:07 +0300

    1siy

--- src/samples/tree_map.py
+++ src/samples/tree_map.py
@@ -1,75 +1,91 @@
-class NodeTree:
-    def __init__(self, key = None, value = None, left = None, right = None):
+from itertools import chain
+
+
+class TreeNode:
+    def __init__(self, key, value, left=None, right=None):
         self.key = key
         self.value = value
         self.left = left
         self.right = right
 
-class HashTree:
-    def __init__(self):
-        self.head = None
-        self.length = 0
+    def __str__(self):
+        return f"TreeNode(key={self.key}, value={self.value}"
 
-    @classmethod
-    def add(cls, a, key, value):
-        if key > a.key:
-            if a.right is None:
-                a.right = NodeTree(key, value)
-            else:
-                cls.add(a.right, key, value)
-        elif key < a.key:
-            if a.left is None:
-                a.left = NodeTree(key, value)
-            else:
-                cls.add(a.left, key, value)
-        else:
-            a.value = value
+
+class TreeMap:
+    def __init__(self):
+        self.root = None
 
     def __setitem__(self, key, value):
-        if self.head == None:
-            self.head = NodeTree(key,value)
-        else:
-            a = self.head
-            self.add(a, key, value)
-            self.length += 1
-    def __getitem__(self, key):
-        a = self.head
-        while a:
-            if a.key == key:
-                return a.value
+        def inner_setitem(node):
+            if node is None:
+                return TreeNode(key, value)
             else:
-                if key > a.key:
-                    a = a.right
+                if key == node.key:
+                    node.value = value
+                elif key < node.key:
+                    node.left = inner_setitem(node.left)
                 else:
-                    a = a.left
-        return None
+                    node.right = inner_setitem(node.right)
+                return node
+        self.root = inner_setitem(self.root)
+
+    def __getitem__(self, key):
+        def inner_getitem(node):
+            if node is None:
+                raise KeyError
+            if key == node.key:
+                return node.value
+            elif key < node.key:
+                return inner_getitem(node.left)
+            else:
+                return inner_getitem(node.right)
+        return inner_getitem(self.root)
+
+    @staticmethod
+    def find_min_node(node):
+        if node.left is not None:
+            return TreeMap.find_min_node(node.left)
+        return node
 
     def __delitem__(self, key):
-        def del_elem(elem,key):
-            if elem:
-                if elem.key == key:
-                    if elem.left:
-                        a = elem.left
-                        while a.right:
-                            if a.right.right == None:
-                                break
-                            a = a.right
-                        if a.right:
-                            a.right.right = elem.right
-                            a.right.left,a.right = elem.left,a.right.left
-                        else:
-                            a.right = elem.right
-                        return a
-                    elif elem.right:
-                        return elem.right
-                    else:
-                        return None
+        def inner_delitem(node, key):
+            if node is None:
+                raise KeyError
+            if key < node.key:
+                node.left = inner_delitem(node.left, key)
+                return node
+            elif key > node.key:
+                result = inner_delitem(node.right, key)
+                node.right = result
+                return node
+            else:
+                if node.left is None and node.right is None:
+                    return None
+                elif node.left is not None and node.right is None:
+                    return node.left
+                elif node.left is None and node.right is not None:
+                    return node.right
                 else:
-                    if elem.key > key:
-                        elem.left = del_elem(elem.left, key)
-                        return elem
-                    else:
-                        elem.right = del_elem(elem.right, key)
-                        return elem
-            return elem
-        self.head = del_elem(self.head,key)
+                    min_node = TreeMap.find_min_node(node.right)
+                    node.key = min_node.key
+                    node.value = min_node.value
+                    node.right = inner_delitem(node.right, min_node.key)
+                    return node
+        self.root = inner_delitem(self.root, key)
+
+    def __str__(self):
+        nodes = [self.root]
+        lines = []
+        while any(nodes):
+            lines.append('\t'.join(str(node and node.key) for node in nodes))
+            nodes = list(chain.from_iterable([node and node.left, node and node.right] for node in nodes))
+        return "\n".join(lines)
+
+    def __iter__(self):
+        def iter_node(node):
+            if node is not None:
+                yield from iter_node(node.left)
+                yield node.key, node.value
+                yield from iter_node(node.right)
+        yield from iter_node(self.root)
